91b03657c8aa53e28fd963845b3df3dcf65aab63
ttolentino89 <trishatolentino89@gmail.com>
2020-04-07 13:04:48 -0400

    item scaffolding added

---
+++ db/migrate/20200407170054_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.string :description
+      t.string :img_link
+      t.string :quantity
+
+      t.timestamps
+    end
+  end
+end
