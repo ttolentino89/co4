619512d8e395c705cc76407bf6139dfb4ba056f7
ttolentino89 <trishatolentino89@gmail.com>
2020-04-06 22:19:47 -0400

    basic auth complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
 
     if @user.save
       @token = encode({user_id: @user.id, username: @user.username});
-      render json: {user: @user, token: @token}, status: :created, location: @user
+      render json: {user: @user.frontend_data, token: @token}, status: :created, location: @user
     else
       render json: @user.errors, status: :unprocessable_entity
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,14 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, length: { minimum: 6 }
-  
+
+  def frontend_data
+    {
+      id: id,
+      username: username,
+      email: email,
+      created_at: created_at,
+      updated_at: updated_at
+    }
+  end
 end
