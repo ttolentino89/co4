8fa1200ce5d7479d933694f936621b03a01e29f1
ttolentino89 <trishatolentino89@gmail.com>
2020-04-07 11:42:17 -0400

    attempting to fix auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
+  # before_action :set_user, only: [:show, :update, :destroy]
+  before_action :authorize_request, except: :create
 
   # GET /users
   def index
@@ -18,7 +19,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      @token = encode({user_id: @user.id, username: @user.username});
+      @token = encode({user_id: @user.id});
       render json: {user: @user.frontend_data, token: @token}, status: :created, location: @user
     else
       render json: @user.errors, status: :unprocessable_entity
