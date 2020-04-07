8f1f35e2f4c23108692e2d26ec9819db4f59a15a
ttolentino89 <trishatolentino89@gmail.com>
2020-04-07 14:04:55 -0400

    change item description from string to text

--- db/migrate/20200407170054_create_items.rb
+++ db/migrate/20200407170054_create_items.rb
@@ -2,9 +2,10 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
       t.string :name
-      t.string :description
+      t.text :description
       t.string :img_link
       t.integer :quantity
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
