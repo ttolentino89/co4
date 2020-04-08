0b5c35c62b61fc4ef688b455452866f610258e37
ttolentino89 <trishatolentino89@gmail.com>
2020-04-07 21:21:54 -0400

    renamed type back to seed, rolled back + seeded categories

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2020_04_07_203858) do
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
-    t.string "type"
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,27 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   movies = Movie.create([{ : 'Star Wars' }, { : 'Lord of the Rings' }])
+#   Character.create(: 'Luke', movie: movies.first)
+
+# Item.create!(
+#   : "Toilet Paper",
+#   description: "Basic American household staple.",
+#   img_link: "https://images.pexels.com/photos/3962433/pexels-photo-3962433.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+#   quantity: 8,
+#   user: users.first,
+# )
+
+Category.create!(name:"basic/universal")
+Category.create!(name:"advanced/special-use")
+Category.create!(name:"household/kitchen")
+Category.create!(name:"outdoor/survival")
+Category.create!(name:"medical/first aid")
+Category.create!(name:"educational")
+Category.create!(name:"entertainment")
+Category.create!(name:"food/drink")
+Category.create!(name:"protective gear")
+Category.create!(name:"tools/hardware")
+Category.create!(name:"miscellaneous")
+
+p "#{Category.count} categories created!"
