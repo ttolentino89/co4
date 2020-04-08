006f639c885dad5cf36d312a9551dfaec72386b2
ttolentino89 <trishatolentino89@gmail.com>
2020-04-08 16:26:13 -0400

    added dependencies + api helpers

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+    <h1>Welcome to the Corona Covid Community Coalition</h1>
     </div>
   );
 }
---
+++ client/src/services/api-helper.js
@@ -0,0 +1,76 @@
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:3000'
+
+const api = axios.create({
+  baseURL: baseUrl
+})
+
+//=====Auth=====
+
+export const loginUser = async (loginData) => {
+  const resp = await api.post('/auth/login', { auth: loginData })
+  localStorage.setItem('authToken', resp.data.token);
+  api.defaults.headers.common.authorization = `Bearer ${resp.data.token}`
+  return resp.data.user
+}
+
+export const registerUser = async (registerData) => {
+  const resp = await api.post('/users/', { user: registerData })
+  localStorage.setItem('authToken', resp.data.token);
+  api.defaults.headers.common.authorization = `Bearer ${resp.data.token}`
+  return resp.data.user
+}
+
+export const verifyUser = async () => {
+  const token = localStorage.getItem('authToken');
+  if (token) {
+    api.defaults.headers.common.authorization = `Bearer ${token}`
+    const resp = await api.get('/auth/verify');
+    return resp.data
+  }
+  return false
+}
+
+export const removeToken = () => {
+  api.defauls.headers.common.authorization = null;
+}
+
+//=====Items=====
+
+export const readAllItems =  async () => {
+  const resp = await api.get('/items');
+  return resp.data;
+}
+
+export const readItemByID =  async (id) => {
+  const resp = await api.get(`/items/${id}`);
+  return resp.data;
+}
+
+export const createItem =  async (itemData) => {
+  const resp = await api.post('/items/', {item: itemData});
+  return resp.data;
+}
+
+export const updateItem =  async (itemData, id) => {
+  const resp = await api.put(`/items/${id}`, {item: itemData});
+  return resp.data;
+}
+
+export const destroyItem =  async (id) => {
+  const resp = await api.delete(`/items/${id}`);
+  return resp.data;
+}
+
+//=====Categories=====
+
+export const readAllCategories = async () => {
+  const resp = await api.get('/categories');
+  return resp.data;
+}
+
+export const addCategory = async (categoryId, id) => {
+  const resp = await api.get(`/categories/${categoryId}/items/${id}`);
+  return resp.data
+}
