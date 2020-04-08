ddc817cff1347c5748dbb370edfa03fb955a82aa
ttolentino89 <trishatolentino89@gmail.com>
2020-04-07 20:43:27 -0400

    join table migrated + add to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_07_170054) do
+ActiveRecord::Schema.define(version: 2020_04_07_203858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "categories_items", id: false, force: :cascade do |t|
+    t.bigint "item_id", null: false
+    t.bigint "category_id", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name"
