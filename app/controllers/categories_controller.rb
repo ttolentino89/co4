2b494d3285fc6eb82acf3e01552df19855092528
ttolentino89 <trishatolentino89@gmail.com>
2020-04-07 21:51:33 -0400

    removed CRUD functionality from categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -14,29 +14,29 @@ class CategoriesController < ApplicationController
   end
 
   # POST /categories
-  def create
-    @category = Category.new(category_params)
-
-    if @category.save
-      render json: @category, status: :created, location: @category
-    else
-      render json: @category.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /categories/1
-  def update
-    if @category.update(category_params)
-      render json: @category
-    else
-      render json: @category.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /categories/1
-  def destroy
-    @category.destroy
-  end
+  # def create
+  #   @category = Category.new(category_params)
+  #
+  #   if @category.save
+  #     render json: @category, status: :created, location: @category
+  #   else
+  #     render json: @category.errors, status: :unprocessable_entity
+  #   end
+  # end
+  #
+  # # PATCH/PUT /categories/1
+  # def update
+  #   if @category.update(category_params)
+  #     render json: @category
+  #   else
+  #     render json: @category.errors, status: :unprocessable_entity
+  #   end
+  # end
+  #
+  # # DELETE /categories/1
+  # def destroy
+  #   @category.destroy
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
