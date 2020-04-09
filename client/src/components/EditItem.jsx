88d8130c20f52124f873815366004fd309c6a1e8
ttolentino89 <trishatolentino89@gmail.com>
2020-04-08 20:39:47 -0400

    createItem + editItem components added

---
+++ client/src/components/EditItem.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+function EditItem(props) {
+  return (
+    <div>
+      <h3>Create a new item</h3>
+      <form onSubmit={props.handleSubmit}>
+        <p>Image Link:</p>
+        <input
+          type="text"
+          name="photo"
+          value={props.itemForm.img_link}
+          onChange={props.handleFormChange} />
+
+        <p>Item name:</p>
+        <input
+          type="text"
+          name="name"
+          value={props.itemForm.name}
+          onChange={props.handleFormChange} />
+
+        <p>Item Description:</p>
+        <input
+          type="text"
+          name="description"
+          value={props.itemForm.description}
+          onChange={props.handleFormChange} />
+
+        <p>Item Quantity:</p>
+        <input
+          type="number"
+          name="qty"
+          value={props.itemForm.description}
+          onChange={props.handleFormChange} />
+          
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default withRouter(EditItem);
